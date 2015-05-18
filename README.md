4abd19b9dd83ef369973a536ca9f74defba9a476
Sam Thorogood <sam.thorogood@gmail.com>
2015-05-18 16:47:10 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# inert-polyfill
+Polyfill for the `inert` HTML attribute.
+
+The `inert` attribute is an [upcoming feature](https://html.spec.whatwg.org/multipage/interaction.html#inert-subtrees) [of HTML](http://drafts.htmlwg.org/html/master/editing.html#inert-subtrees).
+Its implementation is being tracked by some major browser vendors.
+
+From the HTML spec-
+
+> A node (in particular elements and text nodes) can be marked as inert.
+> When a node is inert, then the user agent must act as if the node was absent for the purposes of targeting user interaction events, may ignore the node for the purposes of text search user interfaces (commonly known as "find in page"), and may prevent the user from selecting text in that node.
+
+While not a replacement for proper state management, web developers can use the `inert` attribute to prevent access to subtrees.
+This could be useful to prevent access to active HTML forms, to enable modal-like popovers, or to block user interaction while awaiting the result of an asynchronous operation.
+
+## Example
+
+```html
+<div inert>
+  <button>I'm unclickable!</button>
+  <input type="text" placeholder="I'm unfocusable!" />
+</div>
+```
+
+## Usage
+
+To use the polyfill, the script just needs to be included in your page.
+There are no other initialization steps.
+
+The polyfill prevents tab-focusing, using the `accessKey` to access an element, and any other approach to focus.
+
+### Installation
+
+You may optionally install via Bower-
+
+    $ bower install inert-polyfill
+
+### Supports
+
+This polyfill works on modern versions of all major browsers. It also supports IE9 and above.
+
+## Limitations
+
+Most limitations revolve around keyboard access and the tab key-
+
+- Positive values of `tabIndex` are ignored, and the polyfill will always tab either directly before or after an `inert` element
+  - Avoid relying on a specific tab order
+- Inert elements at the very start or end of a page may prevent tab access to the browser's chrome
+- Elements within an inert element may still receive `focus` and `blur` events
+
+Other limitations include-
+
+- Content within an inert element may still be searched for (using the browser's Find box) or selected
---
+++ inert-polyfill.js
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2015 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
+window.addEventListener('load', function() {
+  function applyStyle(css) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+    document.body.appendChild(style);
+  }
+  var css = "/*[inert]*/[inert]{position:relative!important;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;pointer-events:none}[inert]::before{content:'';display:block;position:absolute;top:0;left:0;right:0;bottom:0}";
+  applyStyle(css);
+
+  /**
+   * Finds the nearest adjacent Element in the specified direction. If this is
+   * negative, this will include the parent element.
+   *
+   * @param {Element} e to find adjacent of
+   * @param {number} dir to move in (+ve or -ve)
+   * @return {Element} adjacent element
+   */
+  function findAdjacent(e, dir) {
+    var arg = dir < 0 ? 'previousElementSibling' : 'nextElementSibling';
+    while (e && e !== document.documentElement) {
+      var adjacent = e[arg];
+      if (adjacent) {
+        return adjacent;
+      }
+      e = e.parentElement;
+      if (dir < 0) {
+        return e;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Determines whether the specified element is inert, and returns the element
+   * which caused this state. This is limited to, but may include, the body
+   * element.
+   *
+   * @param {Element} e to check
+   * @return {Element} element is made inert by, if any
+   */
+  function madeInertBy(e) {
+    while (e && e !== document.documentElement) {
+      if (e.hasAttribute('inert')) {
+        return e;
+      }
+      e = e.parentElement;
+    }
+    return null;
+  }
+
+  /**
+   * Tries to focus on the passed candidate element. Compares to the previously
+   * focused element.
+   *
+   * @param {!Element} candidate
+   * @param {Element} previous
+   * @return {boolean} if focus was successful
+   */
+  function tryFocus(candidate, previous) {
+    // Ignore elements with -ve tabIndex, as they should not be focusable
+    // by tab.
+    // FIXME: Some browsers (Mac Safari) ignores some elements in tab
+    // ordering, similarly to a default -ve value. This is a user pref,
+    // and we need a way to detect it.
+    if (!(candidate.tabIndex < 0)) {
+      candidate.focus();
+      if (document.activeElement !== previous) { return true; }
+    }
+    return false;
+  }
+
+  // Hold onto the last tab direction: next (tab) or previous (shift-tab). This
+  // can be used to step over inert elements in the correct direction. Mouse
+  // or non-tab events should reset this and inert events should focus nothing.
+  var lastTabDirection = 0;
+  document.addEventListener('keydown', function(ev) {
+    if (ev.keyCode === 9) {
+      lastTabDirection = ev.shiftKey ? -1 : +1;
+    } else {
+      lastTabDirection = 0;
+    }
+  });
+  document.addEventListener('mousedown', function(ev) {
+    lastTabDirection = 0;
+  });
+
+  // The 'focusin' event bubbles, but instead, use 'focus' with useCapture set
+  // to true as this is supported in Firefox. Additionally, target the body so
+  // this doesn't generate superfluous events on document itself.
+  document.body.addEventListener('focus', function(ev) {
+    var target = /** @type {Element} */ (ev.target);
+    var inertElement = madeInertBy(target);
+    if (!inertElement) { return; }
+
+    if (lastTabDirection !== 0) {
+      // If the page has been tabbed recently, then focus the next element
+      // in the known direction (if available). Check that calling focus
+      // actually changes the activeElement.
+      var candidate = inertElement;
+      for (;;) {
+        candidate = findAdjacent(candidate, lastTabDirection);
+        if (!candidate) { break; }
+
+        var q = [candidate];
+        while (q.length) {
+          var next = lastTabDirection < 0 ? q.pop() : q.shift();
+//          if (next.hasAttribute('inert')) { continue; }
+
+          // FIXME: If lastTabDirection is -ve, this should try the children of
+          // 'next' first.
+          if (tryFocus(next, target)) { return; }
+
+          for (var i = 0; i < next.children.length; ++i) {
+            q.push(next.children[i]);
+          }
+        }
+      }
+
+      // FIXME: If a focusable element can't be found here, it's likely to mean
+      // that this is the start or end of the page. Blurring is then not quite
+      // right, as it prevents access to the browser chrome.
+    }
+
+    // Otherwise, immediately blur the targeted element. Technically, this
+    // still generates focus and blur events on the element. This is (probably)
+    // the price to pay for this polyfill.
+    target.blur();
+    ev.preventDefault();
+    ev.stopPropagation();
+  }, true);
+
+  // Use a capturing click listener as both a safety fallback, and to prevent
+  // accessKey access to inert elements.
+  document.addEventListener('click', function(ev) {
+    if (madeInertBy(/** @type {Element} */ (ev.target))) {
+      ev.preventDefault();
+      ev.stopPropagation();
+    }
+  }, true);
+
+});
